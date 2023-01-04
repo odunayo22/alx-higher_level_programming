18caaa1ae7ceae4464832b4a2fcce90cbbf2579b
odunayo22 <shakhordun@gmail.com>
2023-01-04 07:58:06 -0800

    python

---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+print("The Zen of Python, by Tim Peters")
+print("Beautiful is better than ugly.")
+print("Explicit is better than implicit.Explicit is better than implicit.")
+print("Simple is better than complex.")
+print("Complex is better than complicated.")
+print("Flat is better than nested.")
+print("Sparse is better than dense.")
+print("Readability counts.")
+print("Special cases aren't special enough to break the rules.")
+print("Although practicality beats purity.")
